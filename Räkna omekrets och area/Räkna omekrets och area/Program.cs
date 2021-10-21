d387c46309a4fe9094dae8dcb3e965d34c6cb7e6
William Spårell <williamatlasnova@gmail.com>
2021-10-21 10:32:49 +0200

    första försket

---
+++ Räkna omekrets och area/Räkna omekrets och area/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Räkna_omekrets_och_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definiera
+            float sidaA = 3.5f;
+            float sidaB = 2.4f;
+            float sidaC = 2.2f;
+
+            //Bearbeta
+            float omkrets = sidaA + sidaB + sidaC;
+            float area = (sidaB * sidaC) / 2;
+              
+            //Presentera
+            Console.WriteLine("Omkretsen är " + omkrets);
+            Console.WriteLine("Arean är " + area);
+        }
+    }
+}
