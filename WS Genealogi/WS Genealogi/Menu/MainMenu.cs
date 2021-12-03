0c2f50e5a6314a37a0b8d1ac55b5f18f216d618b
William Spårell <williamatlasnova@gmail.com>
2021-12-03 14:24:15 +0100

    List

--- WS Genealogi/WS Genealogi/Menu/MainMenu.cs
+++ WS Genealogi/WS Genealogi/Menu/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WS_Genealogi.Utiles;
 using static WS_Genealogi.Utiles.BasicHelper;
 using static WS_Genealogi.Utiles.PersonHelper;
 
@@ -13,16 +14,36 @@ namespace WS_Genealogi.Menu
         public void StartMenu()
         {
             Write("1. Skapa Person");
-            Write("2. Remove Person");
-            Write("3. Add Parents to person");
+            Write("2. Radera Person");
+            Write("3. Lista Personer");
             string input = Console.ReadLine();
 
             if (input == "1")
                 CreatePerson();
             if (input == "2")
                 DeletePerson();
+            if (input == "3") 
+                ListPerson();
+
             
 
+
+        }
+
+        private static void ListPerson()
+        {
+            OutputHelper output = new();
+            Write("Vill du.....");
+            Write("1. Söka på förnaman");
+            Write("2. Söka på efternamn");
+            Write("3. Lista alla");
+            string input2 = Console.ReadLine();
+            if (input2 == "1")
+                output.SearchFirstName();
+            if (input2 == "2")
+                output.SearchLastName();
+            if (input2 == "3")
+                output.ShowEverybody();
         }
 
         private static void DeletePerson()
---
+++ WS Genealogi/WS Genealogi/Utiles/OutputHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WS_Genealogi.Utiles
+{
+    internal class OutputHelper
+    {
+        //https://github.com/marcusjobb/net21/blob/main/OOA/SnyggkodDemo/SnyggkodDemo/SlarvigKod/HeroesAndVillians.cs
+        private readonly List<Models.Person> names = new();
+
+        public void ShowEverybody()
+        {
+            DisplayList(names, "Everybody");
+        }
+        public void SearchLastName()
+        {
+            Console.WriteLine("Search last name");
+            Console.Write("Lastname: ");
+            var input3 = Console.ReadLine();
+            FindLastName(input3);
+        }
+        private void FindLastName(string input3)
+        {
+            var lname = names.Where(x => x.LastName.Contains(input3, StringComparison.InvariantCultureIgnoreCase)).ToList();
+            DisplayList(lname, "Last name contains " + input3);
+        }
+
+        public void SearchFirstName()
+        {
+            Console.WriteLine("Search name");
+            Console.Write("Name: ");
+            var input2 = Console.ReadLine();
+            FindFirstName(input2);
+        }
+        private void FindFirstName(string input2)
+        {
+            var fname = names.Where(x => x.Name.Contains(input2, StringComparison.InvariantCultureIgnoreCase)).ToList();
+            DisplayList(fname, "First name contains " + input2);
+        }
+
+        private static void DisplayList(List<Models.Person> names, string name)
+        {
+            Console.WriteLine(name);
+            foreach (var person in names.OrderBy(n => n.LastName).ThenBy(n => n.Name))
+            {
+                Console.WriteLine(person);
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
