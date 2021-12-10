f81aca22b5cf54001539f9c3ea1c10261c2e84ef
William Spårell <williamatlasnova@gmail.com>
2021-12-10 09:02:11 +0100

    yes

---
+++ Miniräknare/Miniräknare/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniräknare
+{
+    internal class Calculator
+    {
+        double memory= 0;
+        char op = '+';
+        public Calculator Digit (double d) 
+        {
+            switch (op)
+            {
+                case '+': memory += d; break;
+                case '-': memory -= d; break;
+                case '*': memory *= d; break;
+                case '/': memory /= d; break;
+                case 'C':
+                case 'c': memory = 0; op = '+'; break;
+            }
+            return this;
+
+        }
+        public Calculator Operator(char op) 
+        { 
+            this.op = op;
+            return this; 
+        }
+        public double Result() 
+        {  
+            return memory;
+        }
+    }
+}
---
+++ Miniräknare/Miniräknare/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Miniräknare;
+
+var calc = new Calculator();
+var res = calc.Digit(5).Operator('+').Digit(6).Result();
+
+Console.WriteLine(res);
